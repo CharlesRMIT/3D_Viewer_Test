6c3f7392c41528d2475f467a0058c95f137b23a5
Charles Sarah <S3943529@Student.rmit.edu.au>
2025-05-05 10:26:29 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// LTI Launch Endpoint
+app.post('/launch', (req, res) => {
+  console.log('Received LTI Launch:', req.body);
+
+  // In production, you’d validate OAuth here.
+
+  // Render a simple 3D viewer page
+  res.sendFile(path.join(__dirname, 'public', 'viewer.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
